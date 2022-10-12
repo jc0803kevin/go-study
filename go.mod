102489645706da90bf96fd0076325545e021e74a
xiao <jc0803kevin@163.com>
2022-10-12 16:57:52 +0800

    relay

--- go.mod
+++ go.mod
@@ -4,10 +4,17 @@ go 1.16
 
 require (
 	github.com/gdamore/tcell/v2 v2.5.3
+	github.com/go-basic/uuid v1.0.0
+	github.com/gogo/protobuf v1.3.2
 	github.com/ipfs/go-log/v2 v2.5.1
 	github.com/libp2p/go-libp2p v0.22.0
 	github.com/libp2p/go-libp2p-core v0.20.1
 	github.com/libp2p/go-libp2p-pubsub v0.8.1
+	github.com/libp2p/go-reuseport v0.2.0
 	github.com/multiformats/go-multiaddr v0.7.0
+	github.com/mwitkow/go-proto-validators v0.3.2
 	github.com/rivo/tview v0.0.0-20220916081518-2e69b7385a37
+	github.com/scholar-ink/protoc-gen-swagger v0.0.0-20190523024502-b4df36786abc
+	google.golang.org/genproto v0.0.0-20200825200019-8632dd797987
+	google.golang.org/protobuf v1.28.1
 )
---
+++ libp2p/relay/V2.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/peerstore"
+	"github.com/libp2p/go-libp2p-core/pnet"
+	"github.com/libp2p/go-libp2p/p2p/transport/tcp"
+	"log"
+	"os"
+)
+
+
+// 两个节点交流
+
+
+func V2()  {
+
+	ctx , cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	file, err := os.Open("E:\\swarm.key")
+	if err != nil {
+		log.Printf("加载 swarm key失败 %s", err)
+	}
+	psk , err := pnet.DecodeV1PSK(file)
+
+	opts := []libp2p.Option{
+		libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"),
+		//libp2p.DisableRelay(),
+
+		//quic.NewTransport failed: QUIC doesn't support private networks yet
+		libp2p.Transport(tcp.NewTCPTransport),
+		libp2p.PrivateNetwork(psk),
+		libp2p.EnableRelay(),
+	}
+
+	h1, err := libp2p.New(opts...)
+	if err != nil {
+		log.Printf("Failed to create h1: %v", err)
+		return
+	}
+
+	h2, err := libp2p.New(opts...)
+	if err != nil {
+		log.Printf("Failed to create h2: %v", err)
+		return
+	}
+
+	h1.Peerstore().SetAddrs(h2.ID(), h2.Addrs(), peerstore.PermanentAddrTTL)
+	h2.Peerstore().SetAddrs(h1.ID(), h1.Addrs(), peerstore.PermanentAddrTTL)
+
+
+	log.Printf("h1 peers : %s", h1.Peerstore().Peers().String())
+	log.Printf("h2 peers : %s", h2.Peerstore().Peers().String())
+
+	h2.SetStreamHandler("kevin", func(s network.Stream) {
+		rw := bufio.NewReadWriter(bufio.NewReader(s),bufio.NewWriter(s))
+		for{
+			str, _ := rw.ReadString('\n')
+
+			log.Printf("h2 读取到数据  --》 %s", str)
+		}
+	})
+
+	h1stream, err := h1.NewStream(ctx, h2.ID(), "kevin")
+	for i := 0; i< 5 ; i++  {
+		str := fmt.Sprintf("h1 send maessge : %d \n", i)
+		log.Println(str)
+		h1stream.Write([]byte(str))
+	}
+
+	select {}
+}
